8204938fe35c63421efd382b576f314da6807207
Peter <peter1010@users.noreply.github.com>
2014-06-05 21:00:54 +0100

    Fixed errors found when run through pyflakes

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
 import os
-import pwd
 import subprocess
 
 from distutils.core import setup
-from distutils.command import install_data, install
+from distutils.command import install
 
 
 class my_install(install.install):
@@ -16,26 +15,27 @@ class my_install(install.install):
                 print("Setup.py starting the services")
                 from py_src import service
                 service.start_service()
-        return retVal;
+        return retVal
 
 
 def get_batt_checker_exe():
     subprocess.call(['make', '-C', 'c_src'])
-    return os.path.join("c_src","__%s__" % os.uname().machine,"batt_checker")
+    return os.path.join("c_src", "__%s__" % os.uname().machine, "batt_checker")
 
 
-setup(name='batt_checker',
-      version='1.0',
-      description="Check battery levels",
-      url='https://github.com/peter1010/track_my_ip',
-      author='Peter1010',
-      author_email='peter1010@localnet',
-      license='GPL',
-      package_dir={'batt_checker':'py_src'},
-      packages=['batt_checker'],
-      data_files=[\
-            ('/usr/lib/systemd/system',
-                ('batt_checker.timer', 'batt_checker.service')),
-            ('/usr/bin/',(get_batt_checker_exe(),))],
-      cmdclass = {'install' : my_install}
-      )
+setup(
+    name='batt_checker',
+    version='1.0',
+    description="Check battery levels",
+    url='https://github.com/peter1010/track_my_ip',
+    author='Peter1010',
+    author_email='peter1010@localnet',
+    license='GPL',
+    package_dir={'batt_checker': 'py_src'},
+    packages=['batt_checker'],
+    data_files=[
+        ('/usr/lib/systemd/system',
+         ('batt_checker.timer', 'batt_checker.service')),
+        ('/usr/bin/', (get_batt_checker_exe(), ))],
+    cmdclass={'install': my_install}
+)
