f68e3a67c10ef4abcad9a534061584759fe6a277
Peter <peter1010@users.noreply.github.com>
2014-09-14 12:01:35 +0100

    Make setup.py work correctly when called from makepkg

--- setup.py
+++ setup.py
@@ -2,9 +2,21 @@
 
 import os
 import subprocess
+import sys
 
 from distutils.core import setup
-from distutils.command import install
+from distutils.command import install, build
+
+def find_c_src():
+    c_src = "c_src"
+    for i in range(5):
+        if os.path.exists(c_src):
+            break
+        c_src = os.path.join("..", c_src)
+    else:
+        sys.exit(1)
+    return c_src
+
 
 
 class my_install(install.install):
@@ -17,10 +29,15 @@ class my_install(install.install):
                 service.start_service()
         return retVal
 
+class my_build(build.build):
+    def run(self):
+        retVal = super().run()
+        subprocess.call(['make', '-C', find_c_src()])
+        return retVal
 
-def get_batt_checker_exe():
-    subprocess.call(['make', '-C', 'c_src'])
-    return os.path.join("c_src", "__%s__" % os.uname().machine, "batt_checker")
+
+def get_batt_checker_exe(do_compile=True):
+   return os.path.join(find_c_src(), "__%s__" % os.uname().machine, "batt_checker")
 
 
 setup(
@@ -37,5 +54,5 @@ setup(
         ('/usr/lib/systemd/system',
          ('batt_checker.timer', 'batt_checker.service')),
         ('/usr/bin/', (get_batt_checker_exe(), ))],
-    cmdclass={'install': my_install}
+    cmdclass={'install': my_install, 'build': my_build}
 )
